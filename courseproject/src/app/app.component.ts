66b466a4a504c7076e17922a8561ad0a58a9fedd
fabianzuniga <fabian.zuniga@traza.cl>
2023-05-08 02:38:31 -0400

    course project ngIf header  test

--- courseproject/src/app/app.component.ts
+++ courseproject/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'courseproject';
+  chooseRecipe = false;
+  chooseShoppingList = false;
+  onOption(opt: number){
+    if (opt === 0){
+      this.chooseRecipe = true;
+      this.chooseShoppingList = false;
+    }else if (opt === 1){
+      this.chooseRecipe = false;
+      this.chooseShoppingList = true;
+    }else {
+      this.chooseRecipe = false;
+      this.chooseShoppingList = false;
+    }
+  }
 }
--- courseproject/src/app/header/header.component.ts
+++ courseproject/src/app/header/header.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  @Output() option = new EventEmitter<number>();
+  onSelect(opt: number) {
+    this.option.emit(opt);
+  }
 }
